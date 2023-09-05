457fa67712645f9eda4e1f865f450fbec1cca141
aseppp <saepudinasep86@gmail.com>
2023-09-05 15:31:31 +0700

    add comment, profile page

--- constants/index.js
+++ constants/index.js
@@ -1,35 +1,43 @@
-import { BiHomeCircle, BiSearchAlt, BiHeart, BiEdit } from 'react-icons/bi';
-import { BsPeople, BsPerson } from 'react-icons/bs';
+import { AiOutlineTag } from "react-icons/ai";
+import { BiHomeCircle, BiSearchAlt, BiHeart, BiEdit } from "react-icons/bi";
+import { BsFillReplyAllFill, BsPeople, BsPerson } from "react-icons/bs";
+import { MdOutlinePeopleAlt } from "react-icons/md";
 
 export const sidebarLinks = [
   {
-    label: 'Home',
-    path: '/',
-    icon: <BiHomeCircle size={'25px'} color="white" />,
+    label: "Home",
+    path: "/",
+    icon: <BiHomeCircle size={"25px"} color="white" />,
   },
   {
-    label: 'Search',
-    path: '/search',
-    icon: <BiSearchAlt size={'25px'} color="white" />,
+    label: "Search",
+    path: "/search",
+    icon: <BiSearchAlt size={"25px"} color="white" />,
   },
   {
-    label: 'Activity',
-    path: '/activity',
-    icon: <BiHeart size={'25px'} color="white" />,
+    label: "Activity",
+    path: "/activity",
+    icon: <BiHeart size={"25px"} color="white" />,
   },
   {
-    label: 'Create Thread',
-    path: '/create-thread',
-    icon: <BiEdit size={'25px'} color="white" />,
+    label: "Create Thread",
+    path: "/create-thread",
+    icon: <BiEdit size={"25px"} color="white" />,
   },
   {
-    label: 'Comunity',
-    path: '/comunity',
-    icon: <BsPeople size={'25px'} color="white" />,
+    label: "Comunity",
+    path: "/comunity",
+    icon: <BsPeople size={"25px"} color="white" />,
   },
   {
-    label: 'Profile',
-    path: '/profile',
-    icon: <BsPerson size={'25px'} color="white" />,
+    label: "Profile",
+    path: "/profile",
+    icon: <BsPerson size={"25px"} color="white" />,
   },
+];
+
+export const profileTabs = [
+  { value: "threads", label: "Threads", icon: <BsFillReplyAllFill /> },
+  { value: "replies", label: "Replies", icon: <MdOutlinePeopleAlt /> },
+  { value: "tagged", label: "Tagged", icon: <AiOutlineTag /> },
 ];
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,8 @@
 
 import User from "../models/user.models";
 import Community from "../models/community.model";
+import Thread from "../models/thread.model";
+
 import { revalidatePath } from "next/cache";
 
 import { connectToDB } from "../mongoose";
@@ -49,5 +51,37 @@ export async function updateUser({ userId, bio, name, path, username, image }: P
     }
   } catch (error: any) {
     throw new Error(`Failed to create/update user: ${error.message}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        {
+          path: "community",
+          model: Community,
+          select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+        },
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id", // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    });
+    return threads;
+  } catch (error) {
+    console.error("Error fetching user threads:", error);
+    throw error;
   }
 }
